179e069d65cd5b94804f4db1664aee39945dd841
Will Curiale <will970212@msn.com>
2020-03-05 12:39:21 -0500

    commit on master

---
+++ Controllers/TodoItemsController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Todo.Api.Services;
+using System;
+
+namespace Todo.Api.Controllers
+{
+    
+    [ApiController]
+    [Route("api/todos")]
+    
+    public class TodoItemsController : ControllerBase
+    {
+        private ITodoRepository repository;
+        public TodoItemsController(ITodoRepository todoRepository)
+        {
+            repository = todoRepository ??
+                throw new ArgumentNullException(nameof(todoRepository));
+        }
+        [HttpGet]
+        public IActionResult GetTodos()
+        {
+            var result = repository.GetAllTodoItems();
+            if(result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
